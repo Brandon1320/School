025ae8f907825bcc0512183281f9638d21fac63b
Daniel Medrano <daniel@quicktortoise.com>
2019-07-20 15:14:24 -0500

    Agregar invalidaciones

--- ScholarSystem.Logic/Repository/GradeBookRepository.cs
+++ ScholarSystem.Logic/Repository/GradeBookRepository.cs
@@ -1,7 +1,18 @@
-﻿namespace ScholarSystem.Logic.Repository
+﻿using MySql.Data.MySqlClient;
+using ScholarSystem.Logic.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScholarSystem.Logic.Repository
 {
-    public class GradeBookRepository
+    public class GradeBookRepository : Data<GradeBook>
     {
         private GradeBookRepository() { }
+
+        public override GradeBook Retrieve(int id) => throw new NotImplementedException();
+        public override GradeBook[] Retrieve() => throw new NotImplementedException();
+        public override GradeBook[] Retrieve(params GradeBook[] items) => throw new NotImplementedException();
+        public override void Save(GradeBook item) => throw new NotImplementedException();
     }
 }
