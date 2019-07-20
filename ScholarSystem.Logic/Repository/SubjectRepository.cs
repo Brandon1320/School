3cebd50d965d2ddaca590fb2b9bcb4c1557d921b
Daniel Medrano <daniel@quicktortoise.com>
2019-07-20 14:54:54 -0500

    Agregar clase SubjectRepository

---
+++ ScholarSystem.Logic/Repository/SubjectRepository.cs
@@ -0,0 +1,18 @@
+﻿using MySql.Data.MySqlClient;
+using ScholarSystem.Logic.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScholarSystem.Logic.Repository
+{
+    public class SubjectRepository : Data<Subject>
+    {
+        private SubjectRepository() : base() { }
+
+        public override Subject Retrieve(int id) => throw new NotImplementedException();
+        public override Subject[] Retrieve() => throw new NotImplementedException();
+        public override Subject[] Retrieve(params Subject[] items) => throw new NotImplementedException();
+        public override void Save(Subject item) => throw new NotImplementedException();
+    }
+}
